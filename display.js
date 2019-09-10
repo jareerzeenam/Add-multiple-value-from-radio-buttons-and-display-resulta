e5321aa1db32ca810004487ac6e247b3fb3b36f4
jareerzeenam <53073083+jareerzeenam@users.noreply.github.com>
2019-09-10 13:05:40 +0530

    Add files via upload

---
+++ display.js
@@ -0,0 +1,9 @@
+function displayContent() {
+
+    var v = document.getElementById("valuecontent").value; //The value is from textbox value=""
+
+    var x = document.getElementById(v);
+
+    x.style.display = "block";
+
+}
